aa59f249c5a6e0956520b9aed6c38b59960e15ab
konagaya <cresta522@gmail.com>
2020-06-23 05:37:36 +0000

    q1 2

--- q1_1.py
+++ q1_1.py
@@ -1,3 +1,11 @@
+'''
+
+4 つの数 a, b, c, d を受け取り, ベクトル (a, b) と (c, d) の内積を計算する関数, 
+inner(a, b, c, d) を書き, 適当な例を使って確かめよ.
+
+
+'''
+
 import numpy as np
 
 def inner(a, b, c, d):
---
+++ q1_2.py
@@ -0,0 +1,6 @@
+import math
+
+def func(x):
+    return x / math.sin(x) + math.cos(x)
+    
+print(func(math.pi))
